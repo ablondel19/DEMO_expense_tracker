b40787bdcc47900fff91d31402ff09014b64c968
ablondel19 <ablondel@student.s19.be>
2023-08-08 09:12:07 +0200

    Basic frontend setup

---
+++ expense_tracker_react/src/context/GlobalState.js
@@ -0,0 +1,42 @@
+import React, { createContext, useReducer } from 'react';
+import GlobalReducer from './GlobalReducer';
+
+// State
+const initialState = {
+  transactions: [],
+};
+
+// Context
+export const GlobalContext = createContext(initialState);
+
+// Provider
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(GlobalReducer, initialState);
+
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
